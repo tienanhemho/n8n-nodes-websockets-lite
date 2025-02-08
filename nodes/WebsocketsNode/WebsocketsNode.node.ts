d6486bb1e1aa63f4b234952a76217a216456b249
imblowsnow <im.blowsnow@gmail.com>
2025-02-08 17:38:39 +0800

    init

--- nodes/WebsocketsNode/WebsocketsNode.node.ts
+++ nodes/WebsocketsNode/WebsocketsNode.node.ts
@@ -1,12 +1,12 @@
 import {
 	INodeType,
 	INodeTypeDescription,
-	NodeApiError,
 	NodeConnectionType,
 	NodeOperationError,
 } from 'n8n-workflow';
-import { io, Socket } from 'socket.io-client';
 import { ITriggerFunctions, ITriggerResponse } from 'n8n-workflow/dist/Interfaces';
+// @ts-ignore
+import WebSocket from 'ws';
 
 export class WebsocketsNode implements INodeType {
 	description: INodeTypeDescription = {
@@ -28,20 +28,42 @@ export class WebsocketsNode implements INodeType {
 				default: '',
 				placeholder: 'ws://example.com/my-namespace',
 				description: 'The URL of the websocket server to connect to',
+				required: true,
 			},
+			// {
+			// 	displayName: 'Auth Token',
+			// 	name: 'token',
+			// 	type: 'string',
+			// 	default: '',
+			// 	description: 'auth token',
+			// },
+			// {
+			// 	displayName: 'Event Name',
+			// 	name: 'eventName',
+			// 	type: 'string',
+			// 	default: '',
+			// 	description: 'The name of the event to listen for',
+			// },
 			{
-				displayName: 'Auth Token',
-				name: 'token',
-				type: 'string',
-				default: '',
-				description: 'auth token',
-			},
-			{
-				displayName: 'Event Name',
-				name: 'eventName',
-				type: 'string',
-				default: '',
-				description: 'The name of the event to listen for',
+				displayName: 'Return Data Type',
+				name: 'returnDataType',
+				type: 'options',
+				options: [
+					{
+						name: 'string',
+						value: 'string',
+					},
+					{
+						name: 'json',
+						value: 'json',
+					},
+					{
+						name: 'binary',
+						value: 'binary',
+					},
+				],
+				default: 'string',
+				description: '返回的数据格式',
 			},
 			{
 				displayName: 'Init Send Data',
@@ -62,82 +84,70 @@ export class WebsocketsNode implements INodeType {
 
 	async trigger(this: ITriggerFunctions): Promise<ITriggerResponse> {
 		const websocketUrl = this.getNodeParameter('websocketUrl', '') as string;
-		const eventName = this.getNodeParameter('eventName', '') as string;
-		const token = this.getNodeParameter('token', '') as string;
 		const initData = this.getNodeParameter('initData', '') as string;
+		const returnDataType = this.getNodeParameter('returnDataType', 'string') as string;
 		if (!websocketUrl) {
 			throw new NodeOperationError(this.getNode(), '未配置websocketUrl');
 		}
 
-		const socket: Socket = io(websocketUrl, {
-			reconnectionDelayMax: 10000,
-			auth: {
-				token: token,
-			},
+		const socket = new WebSocket(websocketUrl, {});
+
+		console.log('init trigger websocketUrl', websocketUrl);
+
+		const transformData = async (data: any) => {
+			if (returnDataType === 'json') {
+				return JSON.parse(data);
+			}
+			if (returnDataType === 'binary') {
+				return this.helpers.prepareBinaryData(data);
+			}
+			return data.toString("utf8");
+		}
+
+		socket.on('message', async (data: any) => {
+			const resultData = {event: 'message', data: await transformData(data)};
+			this.emit([this.helpers.returnJsonArray([resultData])]);
 		});
 
-		// Connect to the WebSocket server
-		socket.connect();
+		let pingTimer: boolean | any = false;
 
-		if (eventName) {
-			// Listen for the specified event
-			socket.on(eventName, (data: any) => {
-				const resultData = { event: eventName, data };
-				this.emit([this.helpers.returnJsonArray([resultData])]);
-			});
-		} else {
-			socket.onAny((event, ...args) => {
-				const resultData = { event: eventName, data: args };
-				this.emit([this.helpers.returnJsonArray([resultData])]);
-			});
-		}
+		socket.on('open', () => {
+			const resultData = { event: 'open' };
+			this.emit([this.helpers.returnJsonArray([resultData])]);
 
-		let pingTimer : boolean | any = false;
-		if (initData) {
-			socket.on('connect', () => {
+			if (initData) {
 				socket.send(initData);
-
-				if (pingData){
-					pingTimer = setInterval(() => {
-						socket.send(pingData);
-					}, 5000);
-				}
-			});
-		}
+			}
+			if (pingData) {
+				pingTimer = setInterval(() => {
+					socket.send(pingData);
+				}, 5000);
+			}
+		});
 
 		const pingData = this.getNodeParameter('pingData', '') as string;
 
-
 		// Handle connection errors
-		socket.on('connect_error', (error) => {
-			if (pingTimer){
+		socket.on('error', (error: any) => {
+			if (pingTimer) {
 				clearInterval(pingTimer);
 			}
+			console.error('WebSocket connection error', error);
 			const errorData = {
 				message: 'WebSocket connection error',
 				description: error.message,
 			};
-			throw new NodeApiError(this.getNode(), errorData);
-		});
-
-		socket.on("disconnect", (reason) => {
-			if (pingTimer){
-				clearInterval(pingTimer);
-			}
-			const errorData = {
-				message: 'WebSocket Disconnect',
-				description: reason,
-			};
-			throw new NodeApiError(this.getNode(), errorData);
+			throw new NodeOperationError(this.getNode(), errorData);
 		});
 
 		return {
 			closeFunction: async () => {
-				if (pingTimer){
+				if (pingTimer) {
 					clearInterval(pingTimer);
 				}
-				socket.disconnect();
+				socket.close();
 			},
 		};
-	}
+	};
+
 }
